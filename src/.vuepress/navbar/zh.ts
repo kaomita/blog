064d8d7755c5f9a0b3867e0ee3f0cee4eee93699
kaomita <yuan1995309@163.com>
2024-09-30 13:22:01 +0000

    write edit ubuntu

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -14,6 +14,7 @@ export const zhNavbar = navbar([
         prefix: "Ubuntu/",
         children: [ 
           { text: "安装 Ubuntu20.04 服务器", icon: "pen-to-square", link: "1" },
+          { text: "配置 Ubuntu20.04 服务器", icon: "pen-to-square", link: "2" },
         ],
       },
       {
